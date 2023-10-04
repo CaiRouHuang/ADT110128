7e2764b948e95ad1ef5872a645e36c4ab1650d3f
CaiRouHuang <107344463+CaiRouHuang@users.noreply.github.com>
2023-10-04 15:58:27 +0800

    Add files via upload

---
+++ 20231004_prg02.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+for i in range(1,10):
+    for j in range(1,10):
+        ans = i*j;
+        print('%d * %d =%2d' %(i,j,ans),end='  ')
+        
+    print()
